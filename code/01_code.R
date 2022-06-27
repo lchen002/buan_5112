463cc7494c15a3e1d0d885e230a991f54fa2efae
Lei Chen <lchen4@seattleu.edu>
2022-06-27 13:17:13 -0700

    created plots and found mean and median

--- code/01_code.R
+++ code/01_code.R
@@ -9,3 +9,32 @@ View(nfhs)
 
 #subset data to include only var between hhid and shstruc
 hh <- select(nfhs, hhid : shstruc)
+
+#plot distribution of household size for nfhs
+ggplot(data = nfhs) +
+  geom_histogram(mapping = aes(x = hv009), binwidth = 1) +
+  xlab("Household Size")
+
+#create a new data frame that contains only urban households
+urban <- filter(nfhs, hv025 == 1)
+
+#boxplot of distribution of household size by type of urban area
+ggplot(data = urban) +
+  geom_boxplot(mapping = aes(x = hv009, y = hv025, group = hv026)) +
+  coord_flip() +
+  xlab("Household Size") +
+  ylab("Urban Area Type")
+
+#find mean of household size by type of urban area
+urban %>%
+  group_by(hv026) %>%
+  summarise(
+    mean = mean(hv009), n = n()
+  )
+
+#find median of household size by type of urban area
+urban %>%
+  group_by(hv026) %>%
+  summarise(
+    median =median(hv009)
+  )
